f8f0321fb092b9261682934b8b3e72cb8efea7ed
anderson silva <anderson62202896@alunos.sc.senac.br>
2026-03-17 20:46:25 -0300

    ultimos exercicios para enviar

---
+++ listas-em-aula/aula17mar.py
@@ -0,0 +1,4 @@
+frutas = ["maçã", "banana", "uva"]
+numeros = [1, 2, 3, 4]
+
+print("primeira fruta", frutas[0])
---
+++ listas-em-aula/exercicioaula2.py
@@ -0,0 +1,6 @@
+lista_compras = []
+
+lista_compras.append("arroz")
+lista_compras.append("feijão")
+
+print(lista_compras)

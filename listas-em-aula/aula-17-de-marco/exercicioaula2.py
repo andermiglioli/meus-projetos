bd84c5718466be4c919058e2d7ab90a1f491776b
anderson silva <anderson62202896@alunos.sc.senac.br>
2026-03-17 21:16:38 -0300

    exercicios em aula e lista para entregar

---
+++ listas-em-aula/aula-17-de-marco/exercicioaula2.py
@@ -0,0 +1,27 @@
+lista_compras = []
+
+lista_compras.append("arroz")
+lista_compras.append("feijão")
+
+
+print(lista_compras)
+
+perguntas = [
+
+    "Pergunta1? ",
+
+    "Pergunta2? ",
+
+    "Pergunta3? ",
+
+    "Pergunta4? "
+
+]
+
+
+
+for p in perguntas:
+
+    resposta = input(p)
+
+    print("Você respondeu:", resposta)
---
+++ listas-em-aula/aula-17-de-marco/exercicioaula3.py
@@ -0,0 +1,13 @@
+dias = ("segunda", "terça", "quarta", "quinta", "sexta", "sabado", "domingo")
+print(dias)
+print(dias[0])
+print(dias[-1])
+
+#verificando tamanho da tupla
+print(len(dias))
+
+#slicing em tuplas
+print("primeiros 3 dias", dias[:3])
+
+#verificar se um elemento está na tupla
+print("tem segunda ?", "segunda" in dias)
